ba213973c994d8072536657c36b9e74bd14d8366
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-17 02:34:51 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#include stdio.h
+#include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *
+ * Return: Always 0 (Success)
  */
-
-
+int main(void)
+{
+	puts("\"Proramming is like building a multilingual puzzle");
+	return (0);
+}
