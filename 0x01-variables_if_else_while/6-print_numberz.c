ae027d6c286bf3887fe6534da223b64bbf442397
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-18 21:54:53 +0000

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num = num + 1;
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/print_numberz_forloop.cs
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/** int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	printf("\n");
+	return (0); **/
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return(0);
+}
