072007a0711e81bc794fc7084915c92742fe788d
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-18 05:55:27 +0000

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	printf("\n");
+	return (0);
+}
