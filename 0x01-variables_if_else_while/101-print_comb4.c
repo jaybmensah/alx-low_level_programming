b4a9edd876b76bdba346c616c3f179153ec5597d
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-19 21:37:40 +0000

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1 ; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+
+				if (i != 8 || j != 9 || k != 7)
+				{
+					if (i != 8 || j != 9 || k != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
