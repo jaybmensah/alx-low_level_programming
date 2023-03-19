8d3be450f1fd10d92b3b7ed09261efb542fc1bc6
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-19 17:53:02 +0000

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,29 @@
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = 0;
+	char hexadec = 'a';
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num = num + 1;
+	}
+
+	while (hexadec <= 'f')
+	{
+		putchar(hexadec);
+		hexadec++;
+	}
+
+	putchar('\n');
+	return (0);
+}
