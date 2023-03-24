374f07bcbb4ac8dad3dfe102bcd2a681961eb8a4
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-24 10:43:17 +0000

    Modified betty structure

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - checks for uppercase character.
@@ -9,7 +8,7 @@
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 95)
+	if (c >= 65 && c <= 90)
 	{
 		_putchar(c);
 		return (1);
