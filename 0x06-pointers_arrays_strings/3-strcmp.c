1da2449527372971cefad471a8556113ce2cbc95
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-03-31 20:17:25 +0000

    3. strcmp a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int match = 0;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
-			match = 0;
-
-		else
-			match = 1;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
 
-	return (match);
+	return (0);
 }
