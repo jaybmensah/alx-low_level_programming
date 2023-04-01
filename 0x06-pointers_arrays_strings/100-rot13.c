481c99c63fd8d55bec663864415069682feed526
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-04-01 04:15:49 +0000

    a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @s: Value of string
+ *
+ * Return: Encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char uplow[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char lowup[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0, j = 0; s[i] != '\0' && uplow[j] != '\0'; i++, j++)
+		{
+			if (s[i] == uplow[j])
+			{
+				s[i] = lowup[j];
+				break;
+			}
+		}
+	return (s);
+}
