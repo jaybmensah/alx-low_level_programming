41d0d17b4f55b716b39862fed97b16647d589942
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-04-04 05:40:48 +0000

    Write a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - A function that locates a character in a string.
@@ -15,9 +16,13 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			return (&(s[i]));
+			return (s + i);
 		}
 	}
+	if (*s == c)
+	{
+		return (s);
+	}
 
-	return (0);
+	return NULL;
 }
