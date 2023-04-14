59bec78b7692ef4b3a7eeb459ca4632796cdb03c
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-04-14 02:43:13 +0000

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,31 +13,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *myStr;
 	unsigned int i, j, len = 0, len1 = 0;
 
-	for (i = 0; s1 && s1[i]; i++)
-		len++;
-	for (i = 0; s2 && s2[i]; i++)
-		len1++;
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			len++;
+	if (s2 != NULL)
+		for (i = 0; s2[i] != '\0'; i++)
+			len1++;
 
 	if (n < len1)
-	{
 		myStr = malloc(sizeof(char) * (len + n + 1));
-	}
 	else
-	{
 		myStr = malloc(sizeof(char) * (len + len1 + 1));
-	}
 
-	if (!myStr)
+	if (myStr == NULL)
 		return (NULL);
 
 	for (i = 0; i < len; i++)
 		myStr[i] = s1[i];
 
 	j = 0;
-	for (i = len; n < len1 && i < (len + n); i++, j++)
+	for (i = len; i < len + n && s2[j] != '\0'; i++, j++)
 		myStr[i] = s2[j];
 
 	myStr[i] = '\0';
-
 	return (myStr);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - A function that allocates memory for an array, using
+ * malloc.
+ * @nmed: Value of nmed
+ * @size: Value of size
+ *
+ * Return: Pointer to the new allocated space in memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	return (myStr);
+}
