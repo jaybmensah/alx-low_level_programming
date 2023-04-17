9a51fa49beaec54fc873f2c3a9894c851300c7dc
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-04-17 19:49:01 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Definition of the dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: The dog struct defines the properties of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
