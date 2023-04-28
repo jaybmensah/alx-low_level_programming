7946f53ea694d33e1407a71d959014434fa3e65d
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-04-28 03:08:34 +0000

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,35 +10,25 @@
  *
  * Return: The address of the new element, or NULL if it failed.
  */
-list_t *add_node_end(list_t **head, const char *str)
+st_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new, *temp = *head;
 	unsigned int len = 0;
-
-	/* Calculate the length of the input string */
-	while (str[len] != '\0')
+	
+	while (str[len])
 		len++;
 
-	/* Allocate memory for the new node */
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+	new = malloc(sizeof(list_t));
+	(!new) ? (return (NULL)) : (new->str = strdup(str), new->
+			len = len, new->next = NULL);
 
-	/* Duplicate the string */
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	(*head == NULL) ? (*head = new, 0) : (0);
 
-	/* Assign the length of the input string to the new node */
-	new_node->len = len;
+	while (temp->next)
+		temp = temp->next;
+	
+	temp->next = new;
 
-	/* Add the new node to the beginning of the list */
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
+	return (new);
 }
 
