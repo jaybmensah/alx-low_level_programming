03e32682dd460347a4802fc885aafdc9a2614785
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-05-04 23:25:54 +0000

    Task 6

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+/**
+ * flip_bits - A function that returns the number
+ * of bits needed to flip to get from one number to another
+ * @n: Number one
+ * @m: Number two
+ *
+ * Return: The number of bits needed.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flipped_result = n ^ m;
+	unsigned int needed_bits = 0;
+
+	while (flipped_result != 0)
+	{
+		needed_bits += flipped_result & 1;
+		flipped_result >>= 1;
+	}
+
+	return (needed_bits);
+}
+
