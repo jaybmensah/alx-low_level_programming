7ec56c0bd2cbd8c910c3b71586a78433341176cb
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-05-13 00:06:20 +0000

    Update to main.h and 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
 
@@ -18,6 +15,52 @@ void handle_error(const char *message, const char *filename, int exit_code)
 }
 
 /**
+ * remove_existing_file - Removes the existing file if it exists.
+ * @file_to: The name of the file to be removed.
+ */
+void remove_existing_file(const char *file_to)
+{
+	struct stat st;
+
+	if (stat(file_to, &st) == 0)
+	{
+		if (S_ISREG(st.st_mode))
+		{
+			if (unlink(file_to) == -1)
+				handle_error("Can't remove file", file_to, 99);
+		}
+		else
+		{
+			handle_error("Can't write to", file_to, 99);
+		}
+	}
+}
+
+/**
+ * copy_file_contents - Copies the contents of one file to another file.
+ * @src_fd: The file descriptor of the source file.
+ * @dest_fd: The file descriptor of the destination file.
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
+ */
+void copy_file_contents(int src_fd, int dest_fd,
+const char *file_from, const char *file_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t num_bytes_read, num_bytes_written;
+
+	while ((num_bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		num_bytes_written = write(dest_fd, buffer, num_bytes_read);
+		if (num_bytes_written == -1 || num_bytes_written != num_bytes_read)
+			handle_error("Can't write to", file_to, 99);
+	}
+
+	if (num_bytes_read == -1)
+		handle_error("Can't read from file", file_from, 98);
+}
+
+/**
  * main - Copies the contents of one file to another file.
  * @argc: The number of command-line arguments.
  * @argv: An array of command-line argument strings.
@@ -27,36 +70,27 @@ int main(int argc, char *argv[])
 {
 	const char *file_from;
 	const char *file_to;
-	int src_fd;
-	int dest_fd;
-	char buffer[BUFFER_SIZE];
-	ssize_t num_bytes_read, num_bytes_written;
+	int src_fd, dest_fd;
 
 	if (argc != 3)
-	{
 		handle_error("Usage: cp file_from file_to", "", 97);
-	}
 
 	file_from = argv[1];
 	file_to = argv[2];
 
 	src_fd = open(file_from, O_RDONLY);
+
 	if (src_fd == -1)
 		handle_error("Can't read from file", file_from, 98);
 
+	remove_existing_file(file_to);
+
 	dest_fd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 	if (dest_fd == -1)
 		handle_error("Can't write to", file_to, 99);
 
-	while ((num_bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		num_bytes_written = write(dest_fd, buffer, num_bytes_read);
-		if (num_bytes_written == -1)
-			handle_error("Can't write to", file_to, 99);
-	}
-
-	if (num_bytes_read == -1)
-		handle_error("Can't read from file", file_from, 98);
+	copy_file_contents(src_fd, dest_fd, file_from, file_to);
 
 	if (close(src_fd) == -1)
 		handle_error("Can't close fd", file_from, 100);
