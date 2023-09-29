4abc3f606dcbd16015b8b849794cda43c0e8c872
Lionel J.B Mensah <lioneljbmensah@gmail.com>
2023-09-29 05:26:56 +0000

    Print binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
- * print_binary - A function that prints the binary representation of a
+ * print_binary - A function that prints the binary representation of a 
  * number.
  *
- * @n: Value of number to be converted
+ * @n: Value of number to be converted.
  *
  * Return: Binary number.
  */
 
- void print_binary(unsigned long int n)
- {
+void print_binary(unsigned logn it n)
+{
 	if (n > 1)
 		print_binary(n >> 1);
 
 	_putchar((n & 1) ? '1' : '0');
- }
+}
