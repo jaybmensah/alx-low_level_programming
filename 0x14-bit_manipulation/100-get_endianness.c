ff3a5d7b01032788b375b370776ff3692682b539
jaybmensah <lioneljbmensah@gmail.com>
2023-12-15 01:20:50 +0000

    Get endianness of the system

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -13,15 +13,3 @@ int get_endianness(void)
 
 	return (p[0] == 1);
 }
-
-int main(void)
-{
-	int n;
-
-	n = get_endianness();
-	if (n != 0)
-		printf("little endian\n");
-	else
-		printf("big endian\n");
-	return 0;
-}
