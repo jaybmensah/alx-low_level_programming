072b16ef193796f8c8a3660aee8a3e2632f5e32c
jaybmensah <lioneljbmensah@gmail.com>
2023-12-18 23:38:14 +0000

    a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,46 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file into a string
- * @filename: the file to read
- * @letters: the string to store the file in
+ * read_textfile - A function that reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename : Pointer to the filename to be read.
+ * @letters : Number of letters to be read and printed.
  *
- * Return: 0 on success, -1 on failure
-*/
-
+ * Return: Number of letters it could read and print.
+ * 0 if file cannot be opened, is NULL, write fails or
+ * does not read the expected amount of bytes.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp = fopen(filename, "r");
+	FILE *fptr;
+	ssize_t r_count;
+	char *buff;
 
-	if (fp == NULL)
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
 	{
-		perror(filename);
-		exit(EXIT_FAILURE);
+		free(buff);
+		return (0);
 	}
 
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
+	r_count = fread(buff, sizeof(char), letters, fptr);
 
-	while ((read = getline(&line, &len, fp)) != -1)
+	if (r_count == 0 || write(STDOUT_FILENO, buff,
+		r_count) != r_count)
 	{
-		if (strlen(line) == letters)
-		{
-			printf("%s\n", line);
-		}
+		free(buff);
+		fclose(fptr);
+		return (0);
 	}
 
-	free(line);
-	fclose(fp);
-
-	return (0);
+	free(buff);
+	fclose(fptr);
+	return (r_count);
 }
