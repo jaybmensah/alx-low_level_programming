41c089e6c0ae808c1cf848373b742d859ca27fad
jaybmensah <lioneljbmensah@gmail.com>
2023-12-18 23:45:28 +0000

    Function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,11 +10,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
+
 	fp = fopen(filename, "w");
 	if (fp == NULL)
 	{
 		perror("fopen");
-		return -1;
+		return (-1);
 	}
 	fputs(text_content, fp);
 	fclose(fp);
